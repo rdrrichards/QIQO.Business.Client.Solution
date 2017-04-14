b4550945eecd301682fdfaea5056ebfa15848d3f
R. Richards <rdrrichards@hotmail.com>
2017-04-14 17:33:20 -0500

    Starting some UI tweaks

---
+++ QIQO.Business.Client.Core/Converters/UCWidthConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace QIQO.Business.Client.Core
+{
+    public class UCWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value * .94;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
